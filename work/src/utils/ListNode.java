b925fb56d1c63c7e7a185a8ba8b1e1a1e2e54d78
Jhin <81081853@qq.com>
2022-06-29 00:06:11 +0800

    628 commit

---
+++ work/src/utils/ListNode.java
@@ -0,0 +1,22 @@
+package utils;
+
+/**
+ * @author Jhin
+ * @create 2022-06-14 19:34
+ */
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    public ListNode() {
+    }
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
