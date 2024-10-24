bd794bce7e195458f2849834a44455793066019e
shujingwen001 <shujingwen001@ke.com>
2024-10-24 21:26:28 +0800

    hot206

---
+++ work/src/leetcode100/hot206.java
@@ -0,0 +1,32 @@
+package leetcode100;
+
+import utils.ListNode;
+
+/**
+ * 给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
+ *
+ * https://leetcode.cn/problems/reverse-linked-list/description/?envType=problem-list-v2&envId=2cktkvj
+ */
+public class hot206 {
+
+    /**
+     * 时间复杂度：O(n)，遍历一次
+     * 空间负责度：O(1)，一个辅助指针
+     *
+     * @param head
+     * @return
+     */
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode pre = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = pre;
+            pre = head;
+            head = next;
+        }
+        return pre;
+    }
+}
