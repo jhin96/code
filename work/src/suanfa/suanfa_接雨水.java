6200302234dfe67b87bce2b28d014744320798c0
shujingwen001 <shujingwen001@ke.com>
2025-02-24 23:24:02 +0800

    接雨水

--- work/src/suanfa/suanfa_接雨水.java
+++ work/src/suanfa/suanfa_接雨水.java
@@ -38,4 +38,71 @@ public class suanfa_接雨水 {
         return res;
     }
 
+    public static void main(String[] args) {
+        suanfa_接雨水 func = new suanfa_接雨水();
+        int i = func.method1(new int[]{5,5,1,7,1,1,5,2,7,6});
+        System.out.println(i);
+    }
+
+    /**
+     * 从左往右找第一个大于等于当前柱子的，且不能相邻就能接到雨水，从右往左再来一遍，加起来就是答案
+     * 但是这个处理不了两个问题：
+     * 双重计算：从左到右和从右到左的扫描可能会导致某些区域的雨水被计算两次。（2,0,2这种）
+     * 边界条件：在处理边界条件时，可能会出现一些问题，比如在处理越界时的 i 和 j 的调整
+     * 逻辑错误：在寻找第一个大于等于当前柱子的过程中，可能会忽略一些情况，导致无法正确识别可以接雨水的区域
+     *          （5,5,1,7,1,1,5,2,7,6处理第二个的时候会结束时left和i会是第一个7和6）
+     *
+     * @param height
+     * @return
+     */
+    public int method1(int[] height) {
+        if (height == null || height.length < 3) {
+            return 0;
+        }
+        int res = 0;
+        // 从左往右
+        int i = 0;
+        while (i < height.length) {
+            int left = i;
+            while (i < height.length && height[i] <= height[left]) {
+                i++;
+            }
+            // 处理越界
+            if (i == height.length) {
+                i--;
+            }
+            if (i > left + 1 && height[i] >= height[left]) {
+                // 计算结果
+                for (int m = left; m < i; m++) {
+                    res += (height[left] - height[m]);
+                }
+            }
+            if (left == i) {
+                i++;
+            }
+        }
+
+        // 从右往左
+        int j = height.length - 1;
+        while (j > 0) {
+            int right = j;
+            while (j > 0 && height[j] <= height[right]) {
+                j--;
+            }
+            if (j == -1) {
+                j++;
+            }
+            if (j < right - 1 && height[j] >= height[right]) {
+                // 计算结果
+                for (int m = right; m > j; m--) {
+                    res += (height[right] - height[m]);
+                }
+            }
+            if (right == j) {
+                j--;
+            }
+        }
+        return res;
+    }
+
 }
