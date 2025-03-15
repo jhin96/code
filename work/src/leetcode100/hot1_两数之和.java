f3a8dacaf6343049c0ca34c8db59db4e254d410d
shujingwen001 <shujingwen001@ke.com>
2025-03-15 14:20:19 +0800

    hot1

---
+++ work/src/leetcode100/hot1_两数之和.java
@@ -0,0 +1,38 @@
+package leetcode100;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+ * 你可以假设每种输入只会对应一个答案，并且你不能使用两次相同的元素。
+ * 你可以按任意顺序返回答案。
+ *
+ * https://leetcode.cn/problems/two-sum/?envType=problem-list-v2&envId=2cktkvj
+ *
+ */
+public class hot1_两数之和 {
+
+    /**
+     * 时间复杂度：O(n)
+     * 空间复杂度：O(n)
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] twoSum(int[] nums, int target) {
+        if (nums == null || nums.length < 1) {
+            return new int[0];
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[0];
+    }
+
+}
