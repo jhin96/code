4024563e4712259b603b834348ae9d86fdd47a4a
shujingwen001 <shujingwen001@ke.com>
2025-03-31 15:14:16 +0800

    newhot160_相交链表

---
+++ work/src/newhot/newhot160_相交链表.java
@@ -0,0 +1,20 @@
+package newhot;
+
+import utils.ListNode;
+
+public class newhot160_相交链表 {
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
+        ListNode tmpA = headA;
+        ListNode tmpB = headB;
+        while (tmpA != tmpB) {
+            tmpA = tmpA == null ? headB : tmpA.next;
+            tmpB = tmpB == null ? headA : tmpB.next;
+        }
+        return tmpA;
+    }
+
+}
