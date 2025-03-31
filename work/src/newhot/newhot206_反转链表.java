5c82104d29ecc54babe8b5d4b7fc040ebfb1d388
shujingwen001 <shujingwen001@ke.com>
2025-03-31 15:36:11 +0800

    newhot206_反转链表

---
+++ work/src/newhot/newhot206_反转链表.java
@@ -0,0 +1,21 @@
+package newhot;
+
+import utils.ListNode;
+
+public class newhot206_反转链表 {
+
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode pre = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = pre;
+            pre = head;
+            head = next;
+        }
+        return pre;
+    }
+
+}
