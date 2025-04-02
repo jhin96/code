408f8eb1cdf811e5ab1b3f2e4dd6547606dc512c
shujingwen001 <shujingwen001@ke.com>
2025-04-02 14:47:32 +0800

    newhot199_二叉树的右视图

---
+++ work/src/newhot/newhot199_二叉树的右视图.java
@@ -0,0 +1,37 @@
+package newhot;
+
+import utils.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class newhot199_二叉树的右视图 {
+
+    public List<Integer> rightSideView(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<>();
+        }
+        List<Integer> res = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode poll = queue.poll();
+                if (i == size - 1) {
+                    res.add(poll.val);
+                }
+                if (poll.left != null) {
+                    queue.offer(poll.left);
+                }
+                if (poll.right != null) {
+                    queue.offer(poll.right);
+                }
+            }
+        }
+        return res;
+    }
+
+}
