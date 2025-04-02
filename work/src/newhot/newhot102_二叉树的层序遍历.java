168bf3c2ac21f3eb2e74ce2ae72e22b4f68d6616
shujingwen001 <shujingwen001@ke.com>
2025-04-02 14:22:23 +0800

    newhot102_二叉树的层序遍历

---
+++ work/src/newhot/newhot102_二叉树的层序遍历.java
@@ -0,0 +1,37 @@
+package newhot;
+
+import utils.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class newhot102_二叉树的层序遍历 {
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<>();
+        }
+        List<List<Integer>> res = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> list = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode poll = queue.poll();
+                list.add(poll.val);
+                if (poll.left != null) {
+                    queue.offer(poll.left);
+                }
+                if (poll.right != null) {
+                    queue.offer(poll.right);
+                }
+            }
+            res.add(list);
+        }
+        return res;
+    }
+
+}
