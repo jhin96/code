294ab0c3fb97af4e6b1f5b1461e33277c16efa99
shujingwen001 <shujingwen001@ke.com>
2025-04-09 17:44:19 +0800

    newhot78_子集

---
+++ work/src/newhot/newhot78_子集.java
@@ -0,0 +1,26 @@
+package newhot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class newhot78_子集 {
+
+    public List<List<Integer>> subsets(int[] nums) {
+        if (nums == null || nums.length < 1) {
+            return new ArrayList<>();
+        }
+        List<List<Integer>> res = new ArrayList<>();
+        dfs(nums, res, new ArrayList<>(), 0);
+        return res;
+    }
+
+    public void dfs(int[] nums, List<List<Integer>> res, List<Integer> tmp, int index) {
+        res.add(new ArrayList<>(tmp));
+        for (int i = index; i < nums.length; i++) {
+            tmp.add(nums[i]);
+            dfs(nums, res, tmp, i + 1);
+            tmp.remove(tmp.size() - 1);
+        }
+    }
+
+}
